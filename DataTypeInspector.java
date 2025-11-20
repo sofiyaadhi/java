4638ddeb17c248e5bf38b8bbe28dd983563aeac5
sofiya <adhisofiya@gmail.com>
2025-11-20 11:49:22 +0545

    question3

---
+++ DataTypeInspector.java
@@ -0,0 +1,28 @@
+
+/**
+ * Write a description of class DataTypeInspector here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class DataTypeInspector
+{
+    public static void main(String[] args){
+    int a=10;
+    byte b=15;
+    long c=2000000;
+    short d=3;
+    float f1=11.6f;
+    double e1=999.99;
+    char ch='A';
+    System.out.println(a);
+    System.out.println(b);
+    System.out.println(c);
+    System.out.println(d);
+    System.out.println(f1);
+    System.out.println(e1);
+    System.out.println(ch);
+    
+    
+}
+}
---
+++ Defaultvalues.java
@@ -0,0 +1,10 @@
+
+/**
+ * Write a description of class Defaultvalues here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class Defaultvalues
+{
+}
