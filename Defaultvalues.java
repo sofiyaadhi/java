7b3dd3351d43ca2d6ad37235c87c3cfd164fbb72
sofiya <adhisofiya@gmail.com>
2025-11-20 13:49:22 +0545

    question 4

--- Defaultvalues.java
+++ Defaultvalues.java
@@ -7,4 +7,23 @@
  */
 public class Defaultvalues
 {
+    char g;
+    int c;
+    byte a;
+    long d;
+    short b;
+    float e;
+    double f;
+    boolean bool;
+    public static void main(String[] args){
+    Defaultvalues obj=new Defaultvalues();
+    System.out.println("Byte : "+obj.a);
+    System.out.println("Short: "+obj.b);
+    System.out.println("integer : "+obj.c);
+    System.out.println("long : "+obj.d);
+    System.out.println("float : "+obj.e);
+    System.out.println("double : "+obj.f); 
+    System.out.println("char : "+obj.g);
+    System.out.println("boolean : "+obj.bool);
+}
 }
