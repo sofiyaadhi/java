87b84f145ee7010ab06f25814d36ee852c822466
sofiya <adhisofiya@gmail.com>
2025-11-20 13:58:08 +0545

    question 5

---
+++ literalpractice.java
@@ -0,0 +1,26 @@
+
+/**
+ * Write a description of class literalpractice here.
+ *
+ * @author (your name)
+ * @version (a version number or a date)
+ */
+public class literalpractice
+{
+    public static void main(String[] args){
+    long l=20l;
+    float f=2.0f;
+    char c='\u2764';
+    System.out.println("long variable="+l);
+    System.out.println("float variable="+f);
+    System.out.println("chat variable with unicode="+c);
+    
+    
+    
+    
+    
+    
+    
+    
+    }
+}
